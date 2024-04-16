4e96019cfef0125ec59cea09d97c26da7152753b
Tim Schulze-Hartung <108271660+tim-sh@users.noreply.github.com>
2024-04-16 22:10:43 +0200

    Use pluginVerifier, adjust plugin ID, add smoke test for IDE startup (#25)
    * Revert "build: use pluginVerifier for quality assurance (#22)"
    
    This reverts commit 0db07310f184979768ab4fa2407ebc1dc1977dfb.
    
    * Revert "chore: adjust file tree to package names"
    
    This reverts commit 5e131d98b25f08983b34800682236c57b8769771.
    
    * build: use pluginVerifier for quality assurance
    
    * chore: remove 'intellij' from plugin ID
    
    * test: add smoke test for IDE startup

--- src/main/java/com/sap/cap/cds/intellij/Plugin.java
+++ src/main/java/com/sap/cap/cds/intellij/Plugin.java
@@ -4,7 +4,7 @@ public class Plugin {
 
     /* Overview of all relevant IDs, names, and labels for the plugin itself */
 
-    public static final String ID = "com.sap.cap.cds.intellij"; // Should not be changed. See plugin.xml
+    public static final String ID = "com.sap.cap.cds"; // Should not be changed. See plugin.xml
 
     public static final String PACKAGE = "com.sap.cap.cds.intellij";
 
