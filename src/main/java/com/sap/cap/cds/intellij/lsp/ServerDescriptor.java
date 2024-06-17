718d2c18bf8dd9f36092d230b2ae3e36f72772d7
Tim Schulze-Hartung <108271660+tim-sh@users.noreply.github.com>
2024-06-17 17:08:45 +0200

    feat: check Node.js version before starting LSP Server (#35)

--- src/main/java/com/sap/cap/cds/intellij/lsp/ServerDescriptor.java
+++ src/main/java/com/sap/cap/cds/intellij/lsp/ServerDescriptor.java
@@ -12,6 +12,7 @@ import com.sap.cap.cds.intellij.FileType;
 import com.sap.cap.cds.intellij.util.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.apache.maven.artifact.versioning.ComparableVersion;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -36,6 +37,8 @@ public class ServerDescriptor extends ProjectWideLspServerDescriptor {
     @NotNull
     @Override
     public OSProcessHandler startServerProcess() throws ExecutionException {
+        checkNodeVersion();
+
         OSProcessHandler handler = new OSProcessHandler(getCommandLine()) {
             @Override
             protected BaseOutputReader.@NotNull Options readerOptions() {
@@ -55,18 +58,38 @@ public class ServerDescriptor extends ProjectWideLspServerDescriptor {
         return handler;
     }
 
-    private void handleServerError(int exitValue) {
-        if (exitValue != SERVER_EXIT_NODE_VERSION) {
-            Logger.PLUGIN.error("CDS Language Server exited with code " + exitValue);
-            return;
-        }
+    private void checkNodeVersion() {
+        String requiredVersionStr = "";
         try {
             String serverPkg = new String(Files.readAllBytes(Paths.get(resolve(RELATIVE_SERVER_PKG_PATH))));
-            String version = getPropertyAtPath(serverPkg, new String[]{"engines", "node"}).toString();
-            Logger.PLUGIN.error("CDS Language Server requires Node.js version " + version + ". Please update your Node.js installation.");
+            requiredVersionStr = getPropertyAtPath(serverPkg, new String[]{"engines", "node"}).toString();
         } catch (Exception e) {
-            Logger.PLUGIN.error("Cannot determine version of CDS Language Server. Please make sure the CDS Language Server is installed and available in the PATH.", e);
+            UserError.show("Cannot determine Node.js version required by CDS Language Server.", e);
+            return;
         }
+        requiredVersionStr = requiredVersionStr.replaceAll("[^\\d.]", "");
+
+        String versionStr = "";
+        try {
+            Process process = new ProcessBuilder("node", "--version").start();
+            versionStr = new String(process.getInputStream().readAllBytes()).trim();
+        } catch (Exception e) {
+            UserError.show("Cannot determine Node.js version. Please make sure Node.js is installed and available in the PATH.", e);
+            return;
+        }
+        if (versionStr.startsWith("v")) {
+            versionStr = versionStr.substring(1);
+        }
+
+        ComparableVersion requiredVersion = new ComparableVersion(requiredVersionStr);
+        ComparableVersion version = new ComparableVersion(versionStr);
+        if (version.compareTo(requiredVersion) < 0) {
+            UserError.show("CDS Language Server requires Node.js version %s, but found earlier Node.js version %s. Please update your Node.js installation.".formatted(requiredVersionStr, versionStr));
+        }
+    }
+
+    private void handleServerError(int exitValue) {
+        UserError.show("CDS Language Server exited with code " + exitValue);
     }
 
     public GeneralCommandLine getCommandLine() throws ExecutionException {
