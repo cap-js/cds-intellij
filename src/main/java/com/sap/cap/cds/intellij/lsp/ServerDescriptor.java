a86e9dd47f94a942d48f7ed93b209bfc46bd991a
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2024-09-05 11:52:56 +0200

    chore: remove Node.js version check
    Now done by cds-lsp itself

--- src/main/java/com/sap/cap/cds/intellij/lsp/ServerDescriptor.java
+++ src/main/java/com/sap/cap/cds/intellij/lsp/ServerDescriptor.java
@@ -9,23 +9,18 @@ import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor;
 import com.intellij.platform.lsp.api.customization.LspFormattingSupport;
 import com.intellij.util.io.BaseOutputReader;
 import com.sap.cap.cds.intellij.FileType;
-import com.sap.cap.cds.intellij.util.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.apache.maven.artifact.versioning.ComparableVersion;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static com.intellij.util.PathUtil.getJarPathForClass;
-import static com.sap.cap.cds.intellij.util.JsonUtil.getPropertyAtPath;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class ServerDescriptor extends ProjectWideLspServerDescriptor {
 
     private static final String RELATIVE_SERVER_PATH = "cds-lsp/node_modules/@sap/cds-lsp/dist/main.js";
-    private static final String RELATIVE_SERVER_PKG_PATH = "cds-lsp/node_modules/@sap/cds-lsp/package.json";
     private static final String RELATIVE_MITM_PATH = "cds-lsp/mitm.js";
     private static final String RELATIVE_LOG_PATH = "cds-lsp/stdio.json";
 
@@ -36,8 +31,6 @@ public class ServerDescriptor extends ProjectWideLspServerDescriptor {
     @NotNull
     @Override
     public OSProcessHandler startServerProcess() throws ExecutionException {
-        checkNodeVersion();
-
         OSProcessHandler handler = new OSProcessHandler(getCommandLine()) {
             @Override
             protected BaseOutputReader.@NotNull Options readerOptions() {
@@ -55,36 +48,6 @@ public class ServerDescriptor extends ProjectWideLspServerDescriptor {
             handleServerError(exitValue);
         } catch (RuntimeException e) { /* process still running */ }
         return handler;
-    }
-
-    private void checkNodeVersion() {
-        String requiredVersionStr = "";
-        try {
-            String serverPkg = new String(Files.readAllBytes(Paths.get(resolve(RELATIVE_SERVER_PKG_PATH))));
-            requiredVersionStr = getPropertyAtPath(serverPkg, new String[]{"engines", "node"}).toString();
-        } catch (Exception e) {
-            UserError.show("Cannot determine Node.js version required by CDS Language Server.", e);
-            return;
-        }
-        requiredVersionStr = requiredVersionStr.replaceAll("[^\\d.]", "");
-
-        String versionStr = "";
-        try {
-            Process process = new ProcessBuilder("node", "--version").start();
-            versionStr = new String(process.getInputStream().readAllBytes()).trim();
-        } catch (Exception e) {
-            UserError.show("Cannot determine Node.js version. Please make sure Node.js is installed and available in the PATH.", e);
-            return;
-        }
-        if (versionStr.startsWith("v")) {
-            versionStr = versionStr.substring(1);
-        }
-
-        ComparableVersion requiredVersion = new ComparableVersion(requiredVersionStr);
-        ComparableVersion version = new ComparableVersion(versionStr);
-        if (version.compareTo(requiredVersion) < 0) {
-            UserError.show("CDS Language Server requires Node.js version %s, but found earlier Node.js version %s. Please update your Node.js installation.".formatted(requiredVersionStr, versionStr));
-        }
     }
 
     private void handleServerError(int exitValue) {
