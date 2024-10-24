61ade50ada1029deca00fbb0156c9e0e2faf1c8b
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2024-10-24 16:12:52 +0200

    feat: automatic Node.js version determination

--- src/main/java/com/sap/cap/cds/intellij/lsp/ServerDescriptor.java
+++ src/main/java/com/sap/cap/cds/intellij/lsp/ServerDescriptor.java
@@ -3,31 +3,34 @@ package com.sap.cap.cds.intellij.lsp;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreter;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor;
 import com.intellij.platform.lsp.api.customization.LspFormattingSupport;
 import com.intellij.util.io.BaseOutputReader;
 import com.sap.cap.cds.intellij.FileType;
+import com.sap.cap.cds.intellij.util.NodeJsUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.apache.maven.artifact.versioning.ComparableVersion;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static com.intellij.util.PathUtil.getJarPathForClass;
-import static java.nio.charset.Charset.defaultCharset;
+import static com.sap.cap.cds.intellij.util.JsonUtil.getPropertyAtPath;
+import static com.sap.cap.cds.intellij.util.NodeJsUtil.getInterpreter;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class ServerDescriptor extends ProjectWideLspServerDescriptor {
 
-    private static final String RELATIVE_SERVER_PATH = "cds-lsp/node_modules/@sap/cds-lsp/dist/main.js";
+    private static final String RELATIVE_SERVER_BASE_PATH = "cds-lsp/node_modules/@sap/cds-lsp/";
+    private static final String RELATIVE_SERVER_PATH = RELATIVE_SERVER_BASE_PATH + "dist/main.js";
+    private static final String RELATIVE_SERVER_PKG_PATH = RELATIVE_SERVER_BASE_PATH + "package.json";
     private static final String RELATIVE_MITM_PATH = "cds-lsp/mitm.js";
     private static final String RELATIVE_LOG_PATH = "cds-lsp/stdio.json";
-
-    private boolean started;
 
     public ServerDescriptor(@NotNull Project project, @NotNull String presentableName) {
         super(project, presentableName);
@@ -41,49 +44,40 @@ public class ServerDescriptor extends ProjectWideLspServerDescriptor {
             protected BaseOutputReader.@NotNull Options readerOptions() {
                 return BaseOutputReader.Options.forMostlySilentProcess();
             }
-            @Override
-            protected void onOSProcessTerminated(int exitCode) {
-                super.onOSProcessTerminated(exitCode);
-                if (started && exitCode != 0) {
-                    showUserError(getProcess());
-                }
-            }
         };
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) { /*ignore*/ }
-        checkServerRunning(handler);
+
+        Process process = handler.getProcess();
+        int exitValue;
+        try {
+            exitValue = process.exitValue();
+            handleServerError(exitValue);
+        } catch (RuntimeException e) { /* process still running */ }
         return handler;
     }
 
-    private void checkServerRunning(OSProcessHandler handler) {
-        Process process = handler.getProcess();
-        if (process.isAlive()) {
-            started = true;
-        } else {
-            showUserError(process);
-        }
-    }
-
-    private void showUserError(Process process) {
-        try (BufferedReader reader = process.errorReader(defaultCharset())) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.startsWith("Error: ")) {
-                    UserError.show(line);
-                    return;
-                }
-            }
-        } catch (IOException e) { /* ignore */ }
-        UserError.show("CDS Language Server exited with code " + process.exitValue());
-    }
-
     public GeneralCommandLine getCommandLine() throws ExecutionException {
-        return (isDebugCdsLsp() ? getDebugCommandLine() : getDefaultCommandLine())
+        ComparableVersion requiredVersion = getRequiredNodejsVersion();
+        NodeJsLocalInterpreter interpreter = getInterpreter(requiredVersion);
+        return (isDebugCdsLsp() ? getDebugCommandLine(interpreter) : getDefaultCommandLine(interpreter))
                 // TODO check if this is really needed:
                 // Suppress ANSI escape sequences in cds-compiler output
                 .withEnvironment("NO_COLOR", "1")
                 .withCharset(UTF_8);
+    }
+
+    private static ComparableVersion getRequiredNodejsVersion() {
+        String serverPkgPath = resolve(RELATIVE_SERVER_PKG_PATH);
+        String rawVersion = "";
+        try {
+            String serverPkg = new String(Files.readAllBytes(Paths.get(serverPkgPath)));
+            rawVersion = getPropertyAtPath(serverPkg, new String[]{"engines", "node"}).toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot determine Node.js version required by CDS Language Server at [%s]".formatted(serverPkgPath), e);
+        }
+        return NodeJsUtil.extractVersion(rawVersion);
     }
 
     private static boolean isDebugCdsLsp() {
@@ -94,20 +88,21 @@ public class ServerDescriptor extends ProjectWideLspServerDescriptor {
         return (debug != null) && debug.contains("cds-lsp");
     }
 
-    private static GeneralCommandLine getDefaultCommandLine() {
+    private static GeneralCommandLine getDefaultCommandLine(NodeJsLocalInterpreter interpreter) {
         return new GeneralCommandLine(
-                "node",
+                interpreter.getInterpreterSystemDependentPath(),
                 resolve(RELATIVE_SERVER_PATH),
                 "--stdio"
         ).withEnvironment("CDS_LSP_TRACE_COMPONENTS", "*:verbose");
     }
 
-    private static GeneralCommandLine getDebugCommandLine() {
+    private static GeneralCommandLine getDebugCommandLine(NodeJsLocalInterpreter interpreter) {
+        String nodeJsPath = interpreter.getInterpreterSystemDependentPath();
         return new GeneralCommandLine(
-                "node",
+                nodeJsPath,
                 resolve(RELATIVE_MITM_PATH),
                 resolve(RELATIVE_LOG_PATH),
-                "node",
+                nodeJsPath,
                 "--inspect",
                 resolve(RELATIVE_SERVER_PATH),
                 "--stdio"
@@ -120,6 +115,10 @@ public class ServerDescriptor extends ProjectWideLspServerDescriptor {
                 .getParent()
                 .resolve(relativePath)
                 .toString();
+    }
+
+    private void handleServerError(int exitValue) {
+        UserError.show("CDS Language Server exited with code " + exitValue);
     }
 
     @Override
