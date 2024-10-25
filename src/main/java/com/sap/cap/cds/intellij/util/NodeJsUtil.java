1eb6cf1b154a75aed92b3657f5db3c20b0b2214e
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2024-10-25 11:56:48 +0200

    chore

--- src/main/java/com/sap/cap/cds/intellij/util/NodeJsUtil.java
+++ src/main/java/com/sap/cap/cds/intellij/util/NodeJsUtil.java
@@ -9,7 +9,6 @@ import org.apache.maven.artifact.versioning.ComparableVersion;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.Optional;
 
 public class NodeJsUtil {
