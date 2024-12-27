47070cbb7c897b4a7c9caa3c072ca5fd24b36c58
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2024-12-27 16:22:55 +0100

    chore

--- src/test/java/com/sap/cap/cds/intellij/codestyle/CdsCodeStylePanelFactoryTest.java
+++ src/test/java/com/sap/cap/cds/intellij/codestyle/CdsCodeStylePanelFactoryTest.java
@@ -9,6 +9,8 @@ import java.util.List;
 import static com.intellij.application.options.CodeStyle.createTestSettings;
 import static com.sap.cap.cds.intellij.codestyle.CdsCodeStyleOption.Category.ALIGNMENT;
 import static com.sap.cap.cds.intellij.codestyle.CdsCodeStyleOption.Category.WRAPPING_AND_BRACES;
+import static com.sap.cap.cds.intellij.codestyle.CdsCodeStyleOption.Type.BOOLEAN;
+import static org.junit.Assume.assumeTrue;
 
 public class CdsCodeStylePanelFactoryTest extends LightPlatformTestCase {
 
@@ -21,16 +23,19 @@ public class CdsCodeStylePanelFactoryTest extends LightPlatformTestCase {
     }
 
     public void testCdsCodeStylePanelFactoryAllBoolean() {
-        CdsCodeStylePanelFactory factory = new CdsCodeStylePanelFactory(createTestSettings());
+        CdsCodeStyleOption.Category allBooleanCategory = ALIGNMENT;
+        assumeTrue("Assuming all options in category to be boolean", CdsCodeStyleSettings.OPTIONS.values().stream()
+                .filter(option -> option.category == allBooleanCategory)
+                .allMatch(option -> option.type == BOOLEAN));
 
-        CodeStyleAbstractPanel panel = factory.createTabPanel(ALIGNMENT);
+        CdsCodeStylePanelFactory factory = new CdsCodeStylePanelFactory(createTestSettings());
+        CodeStyleAbstractPanel panel = factory.createTabPanel(allBooleanCategory);
         assertEquals(panel.getClass(), CdsCodeStyleCheckboxesPanel.class);
         panels.add(panel);
     }
 
     public void testCdsCodeStylePanelFactoryNotAllBoolean() {
         CdsCodeStylePanelFactory factory = new CdsCodeStylePanelFactory(createTestSettings());
-
         CodeStyleAbstractPanel panel = factory.createTabPanel(WRAPPING_AND_BRACES);
         assertEquals(panel.getClass(), CdsCodeStyleTabularPanel.class);
         panels.add(panel);
