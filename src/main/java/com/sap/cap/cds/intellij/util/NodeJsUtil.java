0950d1a33ad63bbb82d27bba92c38394f69b0de7
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2025-04-17 15:32:44 +0200

    chore

--- src/main/java/com/sap/cap/cds/intellij/util/NodeJsUtil.java
+++ src/main/java/com/sap/cap/cds/intellij/util/NodeJsUtil.java
@@ -1,15 +1,10 @@
 package com.sap.cap.cds.intellij.util;
 
-import com.intellij.execution.ExecutionException;
 import com.intellij.execution.Platform;
-import com.intellij.execution.configurations.GeneralCommandLine;
 import com.sap.cap.cds.intellij.settings.AppSettings;
 import org.apache.maven.artifact.versioning.ComparableVersion;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Optional;
 
 import static com.sap.cap.cds.intellij.lsp.CdsLspServerDescriptor.REQUIRED_NODEJS_VERSION;
@@ -31,28 +26,13 @@ public class NodeJsUtil {
                 .or(Optional::empty);
     }
 
-    public static ComparableVersion extractVersion(String rawVersion) {
-        return new ComparableVersion(rawVersion.replaceAll("[^0-9.]", ""));
+    public static Optional<ComparableVersion> getVersion(String nodeJsPath) {
+        return CliUtil.executeCli(nodeJsPath, "--version")
+                .map(NodeJsUtil::extractVersion);
     }
 
-    public static Optional<ComparableVersion> getVersion(String nodeJsPath) {
-        try {
-            Process process = new GeneralCommandLine(nodeJsPath, "--version").createProcess();
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String line = reader.readLine();
-                if (line == null) {
-                    Logger.PLUGIN.debug("Failed to read version from [%s]: no output".formatted(nodeJsPath));
-                    return Optional.empty();
-                }
-                return Optional.of(extractVersion(line));
-            } catch (IOException e) {
-                Logger.PLUGIN.error("Failed to read version from [%s]: failed to read output".formatted(nodeJsPath), e);
-                return Optional.empty();
-            }
-        } catch (ExecutionException e) {
-            Logger.PLUGIN.error("Failed to read version from [%s]: failed to start process".formatted(nodeJsPath), e);
-            return Optional.empty();
-        }
+    public static ComparableVersion extractVersion(String rawVersion) {
+        return new ComparableVersion(rawVersion.replaceAll("[^0-9.]", ""));
     }
 
     public static boolean isNodeVersionSufficient(ComparableVersion version) {
