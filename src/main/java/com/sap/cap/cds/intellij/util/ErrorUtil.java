e27567b708479b425e63d99854425cc2390730ff
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2025-04-28 10:51:10 +0200

    docs

--- src/main/java/com/sap/cap/cds/intellij/util/ErrorUtil.java
+++ src/main/java/com/sap/cap/cds/intellij/util/ErrorUtil.java
@@ -6,10 +6,19 @@ import com.intellij.notification.Notifications;
 import com.sap.cap.cds.intellij.CdsPlugin;
 
 public class ErrorUtil {
+    /**
+     * Shows an error message in the IDE.
+     * @param message The error message to show.
+     */
     public static void show(String message) {
         Notifications.Bus.notify(new Notification("com.sap.cap.cds.intellij.notifications", CdsPlugin.TITLE, message, NotificationType.ERROR));
     }
 
+    /**
+     * Shows an error message in the IDE with a cause.
+     * @param message The error message to show.
+     * @param cause The cause of the error.
+     */
     public static void show(String message, Throwable cause) {
         ErrorUtil.show(message.replaceFirst("\\p{Punct}\\s*$", ". Original error: ") + cause.getMessage());
     }
