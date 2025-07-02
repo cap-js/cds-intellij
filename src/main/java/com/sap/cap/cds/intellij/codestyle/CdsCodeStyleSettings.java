515c30268fd4c61077269b01991b58865fa75456
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2025-07-02 22:56:33 +0200

    chore: update sample source

--- src/main/java/com/sap/cap/cds/intellij/codestyle/CdsCodeStyleSettings.java
+++ src/main/java/com/sap/cap/cds/intellij/codestyle/CdsCodeStyleSettings.java
@@ -14,81 +14,79 @@ public class CdsCodeStyleSettings extends CdsCodeStyleSettingsBase {
 
     public static final String SAMPLE_SRC = """
                                             using {
-                                              Person    as User,
-                                              UserIdent as UserID
-                                            } from './persons';
+                                                Employee    as Worker,
+                                                WorkerIdent as WorkerID
+                                            } from './employees';
                                             
-                                            @requires             : 'authenticated-user'  @insertonly
-                                            @PropertyRestrictions : true                  @Searchable : true
-                                            entity Task : managed {
-                                              key id       : /* UUID */ String(20) = 4;
-                                                  assignee :            Association to one User @cds.on.insert /*1*/ :       #user;
-                                                  subTasks :            Composition of many {
-                                                                          id : Integer;
-                                                                        }
-                                                  priority :            Integer = 2;
-                                                  dueOn    :            DateTime                @cds.on.insert       : /*2*/ #now;
+                                            @requires            : 'verified-user'  @insertonly
+                                            @PropertyRestrictions: true             @Searchable: true
+                                            entity Project : managed {
+                                                key projectId : /* UUID */ String(20) = 4;
+                                                    owner     :            Association to one Worker @cds.on.insert /*1*/: #worker;
+                                                    tasks     :            Composition of many {
+                                                                               taskId : Integer;
+                                                                           }
+                                                    urgency   :            Integer    = 2;
+                                                    deadline  :            DateTime                  @cds.on.insert      : /*2*/ #now;
                                             }
                                             
-                                            context Lists {
-                                              entity TaskList    as
-                                                  select from Task as task
-                                                  join ToDo as todo
-                                                    on  todo.taskId =      't' + task.id
-                                                    and task.dueOn  is not null
-                                                  mixin {
-                                                    taskType        : Association to one TaskType
+                                            context Records {
+                                                entity ProjectRecords   as
+                                                        select from Project as project
+                                                        join Tasks as task
+                                                            on  task.projectRef  =      'p' + project.projectId
+                                                            and project.deadline is not null
+                                                        mixin {
+                                                            taskCategory    : Association to one TaskCategory
                                             
-                                                                        on taskType.available = true;
-                                                    rejectionStatus : Association to one Text;
-                                                  }
-                                                  into {
-                                                    task.id : String,
-                                                    task.dueOn    as dueOn,
-                                                    task.assignee as assignee
-                                                  }
-                                                union
-                                                  select from GenericTasks
-                                                  mixin {}
-                                                  into {
-                                                    'none' as id
-                                                  }
-                                                  actions {
-                                                    action print()  returns Integer;
-                                                    function sort() returns array of Task;
-                                                  };
+                                                                                  on taskCategory.active = true;
+                                                            rejectionReason : Association to one Description;
+                                                        }
+                                                        into {
+                                                            project.projectId : String,
+                                                            project.deadline as deadline,
+                                                            project.owner    as owner
+                                                        }
+                                                    union
+                                                        select from GeneralProjects
+                                                        mixin {}
+                                                        into {
+                                                            fn('z', 42) as projectId
+                                                        }
+                                                    actions {
+                                                        action printReport() returns Integer;
+                                                        function sortTasks() returns array of Project;
+                                                    };
                                             
-                                              entity HiddenTasks as
-                                                select from Hidden {
+                                                entity ArchivedProjects as
+                                                    select from Archived {
                                             
                                             
-                                                  // only expose ID
-                                                  id
-                                                }
+                                                        // only expose projectId
+                                                        projectId
+                                                    }
                                             }
                                             
-                                            
-                                            type Number : Integer;
+                                            type Identifier  : Integer;
                                             /**
-                                             * # The Text
+                                             * # The Description
                                              *
-                                             * This is a very delicate sentence as it has few tiny words in it and will wrap if too long
+                                             * This is a very precise sentence as it has minimal key phrases and will format properly if extended.
                                              *
                                              * * * *
                                              *
-                                             * - _italics_ and **bold**
-                                             * - **bold** or _italics_
+                                             *   - _italicized_ and **bold**
+                                             * - **bold** or _italicized_
                                              *
-                                             * 1. good
-                                             * 2. cheap
-                                             *
-                                             *    | name    | age |
-                                             *    | ------- | --: |
-                                             *    | john    |  39 |
-                                             *    | joe     |   2 |
-                                             *    | grandma | 108 |
+                                             * 1. efficient
+                                             *    2. scalable
+                                             *    | name    | tenure |
+                                             *   | ------- | -----: |
+                                             *   | alice   |    10 |
+                                             *  | bob     |3 |
+                                             *  | manager |25 |
                                              */
-                                            type Text : String;
+                                            type Description : String;
                                             
                                             """;
 
@@ -113,6 +111,8 @@ public class CdsCodeStyleSettings extends CdsCodeStyleSettingsBase {
         OPTIONS.put("alignTypes", new CdsCodeStyleOption("alignTypes", BOOLEAN, true, "Align types of elements", "Types of elements", ALIGNMENT, null, List.of("alignColonsBeforeTypes", "alignEqualsAfterTypes", "alignTypesWithinBlock", "alignCompositionStructToRight")));
         OPTIONS.put("alignTypesWithinBlock", new CdsCodeStyleOption("alignTypesWithinBlock", BOOLEAN, true, "Within block", "Types of elements", ALIGNMENT, "alignTypes", List.of()));
         OPTIONS.put("alignValuesInAnnotations", new CdsCodeStyleOption("alignValuesInAnnotations", BOOLEAN, true, "Values", "Annotations", ALIGNMENT, "alignAnnotations", List.of()));
+        OPTIONS.put("argsInNewLine", new CdsCodeStyleOption("argsInNewLine", BOOLEAN, true, "Put multiple parenthesized arguments or filters in a new line", "Other", WRAPPING_AND_BRACES, null, List.of()));
+        OPTIONS.put("boolOpsAtLineEnd", new CdsCodeStyleOption("boolOpsAtLineEnd", BOOLEAN, false, "Position 'and', 'or' operators at line end", "Other", OTHER, null, List.of()));
         OPTIONS.put("cqlKeywordCapitalization", new CdsCodeStyleOption("cqlKeywordCapitalization", ENUM, CqlKeywordCapitalization.LOWER.getId(), "Capitalization style of CQL keywords", "Other", OTHER, null, List.of(), CqlKeywordCapitalization.LOWER, CqlKeywordCapitalization.UPPER, CqlKeywordCapitalization.TITLE, CqlKeywordCapitalization.AS_IS));
         OPTIONS.put("finalNewline", new CdsCodeStyleOption("finalNewline", BOOLEAN, true, "Final newline", "Other", WRAPPING_AND_BRACES, null, List.of()));
         OPTIONS.put("formatDocComments", new CdsCodeStyleOption("formatDocComments", BOOLEAN, false, "Format markdown in doc comments", "Format markdown in doc comments", COMMENTS, null, List.of("maxDocCommentLine")));
@@ -170,6 +170,8 @@ public class CdsCodeStyleSettings extends CdsCodeStyleSettingsBase {
     public boolean alignTypes = true;
     public boolean alignTypesWithinBlock = true;
     public boolean alignValuesInAnnotations = true;
+    public boolean argsInNewLine = true;
+    public boolean boolOpsAtLineEnd = false;
     public int cqlKeywordCapitalization = CqlKeywordCapitalization.LOWER.getId();
     public boolean finalNewline = true;
     public boolean formatDocComments = false;
