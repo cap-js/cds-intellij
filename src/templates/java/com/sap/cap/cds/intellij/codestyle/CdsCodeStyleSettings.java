61973832ebbfb018d4b485c1856beac381407d55
Tim Schulze-Hartung <108271660+tim-sh@users.noreply.github.com>
2025-07-22 13:18:57 +0200

    chore: explicitly check-in code-style settings as template only (#185)
    Thus, upgrading cds-lsp doesn't lead to git changes

---
+++ src/templates/java/com/sap/cap/cds/intellij/codestyle/CdsCodeStyleSettings.java
@@ -0,0 +1,14 @@
+package com.sap.cap.cds.intellij.codestyle;
+
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.sap.cap.cds.intellij.codestyle.CdsCodeStyleOption.Category;
+
+import java.util.List;
+import java.util.Set;
+
+import static com.sap.cap.cds.intellij.codestyle.CdsCodeStyleOption.Category.*;
+import static com.sap.cap.cds.intellij.codestyle.CdsCodeStyleOption.Type.*;
+
+// NOTE: class body is generated
+public class CdsCodeStyleSettings extends CdsCodeStyleSettingsBase {
+}
