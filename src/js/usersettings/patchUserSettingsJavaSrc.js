dd987b9891b79a745cbc18111c9a394a59d812b7
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2025-08-12 16:51:31 +0200

    feat(user-settings): generate user settings method in CdsUserSettings class

---
+++ src/js/usersettings/patchUserSettingsJavaSrc.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const { readFileSync, writeFileSync } = require('node:fs');
+
+const schemaPath = path.resolve(__dirname, '../../../lsp/schemas/user-settings.json');
+const schema = require(schemaPath);
+const settingsFromSchema = schema.properties;
+
+const srcPath = path.resolve(__dirname, '../../../src/templates/java/com/sap/cap/cds/intellij/usersettings/CdsUserSettings.java');
+const tgtPath = path.resolve(__dirname, '../../../src/main/java/com/sap/cap/cds/intellij/usersettings/CdsUserSettings.java');
+const src = readFileSync(srcPath, 'utf8');
+
+function getDefaultValue(config) {
+  const type = config.type;
+  if (type === 'boolean') return config.default;
+  if (type === 'integer') return config.default;
+  if (type === 'array') return `"${Array.isArray(config.default) ? config.default.join(',') : ''}"`;
+  return `"${(config.default || '').replace(/"/g, '\\"')}"`;
+}
+
+const settings = Object.entries(settingsFromSchema)
+  .sort(([key1], [key2]) => key1.localeCompare(key2))
+  .map(([key, config]) => ({
+    key,
+    defaultValue: getDefaultValue(config)
+  }));
+
+const t = '        ';
+
+const getAllSettingsMethod = `public Map<String, Object> getAllSettings() {
+        Map<String, Object> defaults = new HashMap<>();
+${settings.map(s => `${t}defaults.put("${s.key}", ${s.defaultValue});`).join('\n')}
+        return defaults;
+    }`;
+
+const patchedSrc = src.replace(
+    /public Map<String, Object> getAllSettings\(\) \{[\s\S]*?return defaults;\s*\}/,
+    getAllSettingsMethod
+);
+
+writeFileSync(tgtPath, patchedSrc, 'utf8');
+
+console.log('Generated CDS user settings successfully!');
--- src/main/java/com/sap/cap/cds/intellij/usersettings/CdsUserSettings.java
+++ src/main/java/com/sap/cap/cds/intellij/usersettings/CdsUserSettings.java
@@ -1,42 +1,60 @@
 package com.sap.cap.cds.intellij.usersettings;
 
-import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.Service;
 import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@State(
-        name = "com.sap.cap.cds.intellij.usersettings.CdsUserSettings",
-        storages = @Storage("CdsUserSettings.xml")
-)
-public class CdsUserSettings implements PersistentStateComponent<CdsUserSettings.State> {
+@Service(Service.Level.PROJECT)
+public class CdsUserSettings {
 
-    public static class State {
-        // TODO: Generator will populate this with actual fields
+    private final Project project;
+
+    public CdsUserSettings(Project project) {
+        this.project = project;
     }
-
-    private State state = new State();
 
     public static CdsUserSettings getInstance(Project project) {
         return project.getService(CdsUserSettings.class);
     }
 
-    @Override
-    public State getState() {
-        return state;
-    }
-
-    @Override
-    public void loadState(@NotNull State state) {
-        this.state = state;
-    }
-
     public Map<String, Object> getAllSettings() {
-        // TODO: Generator will implement this properly
-        return new HashMap<>();
+        Map<String, Object> defaults = new HashMap<>();
+        defaults.put("cds.codeLensStatistics.enabled", false);
+        defaults.put("cds.compiler.markMissingI18nDefault", false);
+        defaults.put("cds.compiler.showInternalErrors", false);
+        defaults.put("cds.completion.annotations.omitRedundantTypesInSnippets", false);
+        defaults.put("cds.completion.docFiles", "README.md");
+        defaults.put("cds.completion.formatSnippets", false);
+        defaults.put("cds.completion.showDocumentation", true);
+        defaults.put("cds.completion.workspaceSymbols.maxProposals", -1);
+        defaults.put("cds.completion.workspaceSymbols.minPrefixLength", -1);
+        defaults.put("cds.contributions.enablement.additionalAnalyticalAnnotations", false);
+        defaults.put("cds.contributions.enablement.odata", true);
+        defaults.put("cds.contributions.registry", "https://registry.npmjs.org/");
+        defaults.put("cds.diagnosticsSeverity", "Warning");
+        defaults.put("cds.internal.inspectTokens", false);
+        defaults.put("cds.outline.elements.associationComposition", true);
+        defaults.put("cds.outline.semantical", false);
+        defaults.put("cds.quickfix.importArtifact", false);
+        defaults.put("cds.refactoring.files.delete.enabled", true);
+        defaults.put("cds.refactoring.files.rename.enabled", true);
+        defaults.put("cds.semanticHighlighting.enabled", false);
+        defaults.put("cds.semanticHighlighting.odata.enabled", false);
+        defaults.put("cds.typeGenerator.command", "${typerBinary} \"${targetFile}\" --outputDirectory \"${outputDirectory}\"");
+        defaults.put("cds.typeGenerator.enabled", false);
+        defaults.put("cds.typeGenerator.localInstallationOnly", true);
+        defaults.put("cds.typeGenerator.outputPath", "./@cds-models");
+        defaults.put("cds.whereused.showGenericAnnotations", false);
+        defaults.put("cds.whereused.showStringConstants", false);
+        defaults.put("cds.workspace.debounceFastChanges", true);
+        defaults.put("cds.workspace.fastDiagnosticsMode", "Clear");
+        defaults.put("cds.workspace.scanCsn", "BY_FILE_EXTENSION");
+        defaults.put("cds.workspace.scanDependentModules", false);
+        defaults.put("cds.workspaceSymbols.caseInsensitive", false);
+        defaults.put("cds.workspaceSymbols.lazy", false);
+        defaults.put("cds.workspaceValidationMode", "OpenEditorsOnly");
+        return defaults;
     }
 }
