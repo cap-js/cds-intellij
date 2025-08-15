e3411102351e74ba8be768695ac422e5724d2802
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2025-08-15 19:10:52 +0200

    refactor(lsp): replace hardcoded language server ID with constant reference

--- src/main/java/com/sap/cap/cds/intellij/lsp4ij/CdsLanguageServer.java
+++ src/main/java/com/sap/cap/cds/intellij/lsp4ij/CdsLanguageServer.java
@@ -8,6 +8,8 @@ import com.sap.cap.cds.intellij.lspServer.CdsLspServerDescriptor;
 
 public class CdsLanguageServer extends OSProcessStreamConnectionProvider {
 
+    public static final String ID = "cds";
+
     public CdsLanguageServer() {
         super.setCommandLine(CdsLspServerDescriptor.getServerCommandLine());
     }
@@ -15,6 +17,6 @@ public class CdsLanguageServer extends OSProcessStreamConnectionProvider {
     public Object getInitializationOptions(VirtualFile rootUri) {
         // TODO: in LSP accept cds top-level node but also accept current non-cds second-level nodes. Then use cds here and also adapt vscode to send cds.
         //  Idea: LSP pulls settings. This allows to pull non-cds settings if needed
-        return CdsLanguageClient.getInitializationOptions().get("cds");
+        return CdsLanguageClient.getInitializationOptions().get(ID);
     }
 }
