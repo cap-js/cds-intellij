e2f4e5e4f1baeab2f28b0e87a48edccb5c316e54
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2025-10-09 13:11:47 +0200

    fix(formatting): always use configured/default CDS tabSize
    This fixes a bug where, if .cdsprettier.json contained no tabSize entry, the editor tab size (generic user setting) sent with the formatting request was used.

--- src/main/java/com/sap/cap/cds/intellij/lsp4ij/CdsFormattingFeature.java
+++ src/main/java/com/sap/cap/cds/intellij/lsp4ij/CdsFormattingFeature.java
@@ -1,7 +1,11 @@
 package com.sap.cap.cds.intellij.lsp4ij;
 
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
 import com.redhat.devtools.lsp4ij.client.features.LSPFormattingFeature;
+import com.sap.cap.cds.intellij.codestyle.CdsCodeStyleSettings;
+import com.sap.cap.cds.intellij.codestyle.CdsCodeStyleSettingsService;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -29,5 +33,22 @@ public class CdsFormattingFeature extends LSPFormattingFeature {
     public @Nullable String getFormatOnStatementTerminatorCharacters(@NotNull PsiFile file) {
         // Q: ';' ?
         return super.getFormatOnStatementTerminatorCharacters(file);
+    }
+
+    @Override
+    public @Nullable Integer getTabSize(@NotNull PsiFile file, @Nullable Editor editor) {
+        int defaultValue = (int) CdsCodeStyleSettings.OPTIONS.get("tabSize").defaultValue;
+        if (editor == null) {
+            return defaultValue;
+        }
+        Project project = editor.getProject();
+        if (project == null) {
+            return defaultValue;
+        }
+        CdsCodeStyleSettingsService service = project.getService(CdsCodeStyleSettingsService.class);
+        if (service == null) {
+            return defaultValue;
+        }
+        return service.getSettings().tabSize;
     }
 }
