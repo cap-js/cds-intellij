55f4e16304ba465d9fa50c2b248d6b65ca7ca110
Tim Schulze-Hartung <tim.schulze-hartung@sap.com>
2026-02-24 21:53:24 +0100

    chore: remove unnecessary server-side feature

--- src/main/java/com/sap/cap/cds/intellij/lsp4ij/CdsOnTypeFormattingFeature.java
+++
@@ -1,6 +0,0 @@
-package com.sap.cap.cds.intellij.lsp4ij;
-
-import com.redhat.devtools.lsp4ij.client.features.LSPOnTypeFormattingFeature;
-
-public class CdsOnTypeFormattingFeature extends LSPOnTypeFormattingFeature {
-}
